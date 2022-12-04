cc5abde5d0e008347dfbc7662870ad01b5a71fa3
ArtemZEL <artemzelenok60@gmail.com>
2022-12-04 23:07:47 +0300

    add to img and Card

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
 import Button from './Components/Button/Button';
+import Card from './Components/Card/Card';
+const {getData} = require("./db/db");
+
+const foods=getData();
+
 function App() {
   return (
    <>
-   Hi
+   <h1 className="heading">Order food</h1>
    <Button title={'Добавить'} disable={false} type={'add'}/>
    <Button title={'Удалить'} disable={false} type={'remove'}/>
    <Button title={'Проверить'} disable={false} type={'checkout'}/>
-
+    <div className="cards__container">
+    {foods.map(food=>{
+      return <Card food={food} key={food.id}/>
+    })}
+    </div>
+    
    </>
    
   );
